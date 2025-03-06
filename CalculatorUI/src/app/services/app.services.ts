9250a7ada0d1b72485bba35bd831b9fd32a8c68a
elizabeth.adegunwa <elizabeth.adegunwa@purplemedia.co.uk>
2025-03-06 21:51:40 +0000

    commit

--- CalculatorUI/src/app/services/app.services.ts
+++ CalculatorUI/src/app/services/app.services.ts
@@ -1,4 +1,3 @@
-// src/app/services/app.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -11,7 +10,6 @@ export class AppService {
 
   add(start: number, amount: number): Observable<any> {
     return this.http.get(` http://localhost:5101/api/calculator/add?start=${start}&amount=${amount}`);
-    // http://localhost:5101/api/calculator/subtract?start=10&amount=5
   }
   subtract(start: number, amount: number): Observable<any> {
     return this.http.get(` http://localhost:5101/api/calculator/subtract?start=${start}&amount=${amount}`);
