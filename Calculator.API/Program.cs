36fc456ec31d7873e2ce1a05b66d3386c0df95c1
elizabeth.adegunwa <elizabeth.adegunwa@purplemedia.co.uk>
2025-03-06 21:16:31 +0000

    first commit

---
+++ Calculator.API/Program.cs
@@ -0,0 +1,36 @@
+using Calculator.Core.Interfaces;
+using Calculator.Core.Services;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddScoped<ISimpleCalculator, SimpleCalculator>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        b =>
+        {
+            b.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+app.UseCors("AllowAll");
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
+
+
